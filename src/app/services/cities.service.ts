dfaba47a43f8ae3849b93b60a18dd92e0529edfb
Rajat Khare <rajat.khare@pb.com>
2022-01-09 16:46:35 +0530

    Added the City service to make an API call to fetch cities

--- src/app/services/cities.service.ts
+++ src/app/services/cities.service.ts
@@ -1,16 +1,24 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {map, Observable} from "rxjs";
-import {City} from "../shared/City";
+import { HttpClient } from '@angular/common/http';
+import { map, Observable } from 'rxjs';
+import { City } from '../shared/City';
 
+/**
+ * The service to get cities data from the server
+ */
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CitiesService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
+  /**
+   * Get cities data from the server
+   * @return an observable of array of cities
+   */
   getCities(): Observable<City[]> {
-  return this.http.get<{cities: City[] }>('http://localhost:3000/cities').pipe(map(response => response.cities));
+    return this.http
+      .get<{ cities: City[] }>('http://localhost:3000/cities')
+      .pipe(map((response) => response.cities));
   }
 }
