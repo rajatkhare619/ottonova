af72b2dbc431195820fcb52d0e102dd4d4a24251
Rajat Khare <rajat.khare@pb.com>
2022-01-09 19:57:48 +0530

    Fixed unit tests for the City component

--- src/app/city/city.component.spec.ts
+++ src/app/city/city.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CityComponent } from './city.component';
 
 describe('CityComponent', () => {
@@ -8,14 +7,25 @@ describe('CityComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CityComponent ]
-    })
-    .compileComponents();
+      declarations: [CityComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CityComponent);
     component = fixture.componentInstance;
+    component.city = {
+      name: 'Munich',
+      name_native: 'München',
+      country: 'Germany',
+      continent: 'Europe',
+      latitude: 48.137154,
+      longitude: 11.576124,
+      population: 1472000,
+      founded: 1158,
+      landmarks: ['Bavaria statue', 'Marienplatz', 'ottonova office'],
+      image: 'image.jpg',
+    };
     fixture.detectChanges();
   });
 
